a4fe2a2275caf5844c7023aee636fb7eda677998
Ryan Canton <ryancanton999@gmail.com>
2023-02-24 13:43:38 -0700

    Refactor: Change recipe service to call on be app instead of 3rd party api

--- spec/features/discover/show_spec.rb
+++ spec/features/discover/show_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe 'show' do
     it 'shows the details of a specific recipe' do
         json_response = File.read('spec/fixtures/recipe_details.json')
-    stub_request(:get, "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/479101/information")
-      .with(headers: { 'X-RapidAPI-Key' => '4f4fbc3ba2msh745c24cf4e7edbdp13621djsn50bc1816f418', 'X-RapidAPI-Host' => 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com' })
+    stub_request(:get, 'https://babe_whats_for_dinner_be.herokuapp.com/recipes/479101/information')
       .to_return(status: 200, body: json_response, headers: {})
 
         visit "/discover/479101"
