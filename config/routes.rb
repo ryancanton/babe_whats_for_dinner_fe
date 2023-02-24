bd2fefd527ce8b00974589b7864957c86971d95d
Ryan Canton <ryancanton999@gmail.com>
2023-02-24 14:46:42 -0700

    Feat: Handroll GitHub OAuth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :discover, only: [:index, :show]
+  root "welcome#index"
+  get '/auth/github/callback', to: 'sessions#create'
+  get '/dashboard', to: 'dashboard#show'
 end
