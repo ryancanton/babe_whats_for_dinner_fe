dd70de1946a4fbc1bb2a3d44eaa598c0bfab48d5
Ryan Canton <ryancanton999@gmail.com>
2023-02-25 12:56:19 -0700

    Fixes: Changed poros to correctly pull data from json built by be

--- app/facades/recipe_results_facade.rb
+++ app/facades/recipe_results_facade.rb
@@ -1,6 +1,7 @@
 class RecipeResultsFacade
     def self.get_recipe(id)
-      data = RecipeResultsService.get_recipe_info(id)
+      data = RecipeResultsService.get_recipe_info(id)[:data]
+      binding.pry
       Recipe.new(data)
     end
   end
--- app/services/discover_results_service.rb
+++ app/services/discover_results_service.rb
@@ -1,13 +1,12 @@
 class DiscoverResultsService
   def self.get_search_results(params)
-    url = "https://babe-whats-for-dinner-be.herokuapp.com/recipes/findByIngredients?ingredients=" + self.parse(params)
+    url = "http://babe-whats-for-dinner-be.herokuapp.com/api/v1/recipes/findByIngredients?ingredients=" + self.parse(params)
     
     response = Faraday.get(url)
-
     data = JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.parse(params)
-    parsed = params.downcase.gsub(/\s+/, "").gsub(",","%2C")
+    parsed = params.downcase.gsub(/\s+/, "")
   end
 end
