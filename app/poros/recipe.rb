ebb70e5fd7d06b9e74e1d2518ac3697f4270a623
Bryan Flanagan <bflanagan78@yahoo.com>
2023-02-26 13:06:14 -0700

    refactor: update poros recipe.rb extenended ingredients

--- app/poros/recipe.rb
+++ app/poros/recipe.rb
@@ -2,17 +2,19 @@ class Recipe
   attr_reader :id, :title, :ready_in_minutes, :image, :summary, :instructions, :extended_ingredients
 
   def initialize(data)
+    require 'pry'; binding.pry
     @id = data[:id]
     @title = data[:title]
     @ready_in_minutes = data[:readyInMinutes]
     @image = data[:image]
     @summary = data[:summary]
     @instructions = data[:instructions]
-    @extended_ingredients = Recipe.extended_ingredients(data[:extendedIngredients])
+    @extended_ingredients = Recipe.extended_ingredients(data[:attributes][:ingredients])
     
   end
 
   def self.extended_ingredients(data)
+    require 'pry'; binding.pry
     data.map do |ingr|
       {
         name: ingr[:name],
