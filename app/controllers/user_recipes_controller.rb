ed9ae92071af0b034a903aac3b9f55cb815d9c8c
Lacey Weaver <jlweave89@gmail.com>
2023-02-28 10:27:28 -0700

    Add destroy method and tests

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -1,11 +1,13 @@
 class UserRecipesController < ApplicationController
   def create
     Faraday.post("https://babe-whats-for-dinner-be.herokuapp.com/api/v1/user_recipes?uid=#{params[:uid]}&recipe_id=#{params[:recipe_id]}&recipe_name=#{params[:recipe_name]}")
-    if response.status != 404
-      @saved_recipes = UserRecipesFacade.get_recipes(current_user.uid)
-    else
-      @saved_recipes = [1234]
-    end
+    @saved_recipes = UserRecipesFacade.get_recipes(current_user.uid)
+    redirect_to dashboard_path
+  end
+
+  def destroy
+    Faraday.delete("https://babe-whats-for-dinner-be.herokuapp.com/api/v1/user_recipes?uid=#{params[:uid]}&recipe_id=#{params[:recipe_id]}")
+    @saved_recipes = UserRecipesFacade.get_recipes(current_user.uid)
     redirect_to dashboard_path
   end
 end
--- spec/features/dashboard/show_spec.rb
+++ spec/features/dashboard/show_spec.rb
@@ -1,24 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe 'User Dashboard' do
-  it 'contains a list of a users saved recipes that are links to the recipes show page' do
-    user = User.create!(uid: "12345")
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    before (:each) do
+      user = User.create!(uid: "12345")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    json_response = File.read('spec/fixtures/user_recipes.json')
+      json_response = File.read('spec/fixtures/user_recipes.json')
 
-    stub_request(:get, "https://babe-whats-for-dinner-be.herokuapp.com/api/v1/user_recipes?uid=12345").
-         with(
+      stub_request(:get, "https://babe-whats-for-dinner-be.herokuapp.com/api/v1/user_recipes?uid=12345").
+          with(
            headers: {
        	  'Accept'=>'*/*',
        	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
        	  'User-Agent'=>'Faraday v2.7.4'
-           }).
-         to_return(status: 200, body: json_response, headers: {})
-
+           }).to_return(status: 200, body: json_response, headers: {})
+    end
+  it 'contains a list of a users saved recipes that are links to the recipes show page' do
     visit dashboard_path
     
     expect(page).to have_link('Peanut Butter', href: "/discover/5678")
     expect(page).to have_link('Jelly', href: "/discover/5679")
+  end
+
+  it "has a link to delete a favorited recipe" do
+    visit dashboard_path
+
+    json_response = File.read('spec/fixtures/user_recipes2.json')
+
+    stub_request(:get, "https://babe-whats-for-dinner-be.herokuapp.com/api/v1/user_recipes?uid=12345").
+        with(
+          headers: {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent'=>'Faraday v2.7.4'
+          }).to_return(status: 200, body: json_response, headers: {})
+
+    stub_request(:delete, "https://babe-whats-for-dinner-be.herokuapp.com/api/v1/user_recipes?recipe_id=5678&uid=12345").
+         with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Faraday v2.7.4'
+           }).
+         to_return(status: 200, body: "", headers: {})
+    within "div#5678" do
+      expect(page).to have_link('Peanut Butter', href: "/discover/5678")
+      expect(page).to have_button("Delete Recipe")
+
+      click_button("Delete Recipe")
+    end
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to_not have_content('Peanut Butter')
   end
 end
