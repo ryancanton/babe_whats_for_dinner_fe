36ffbaa9864e9dcbdc02f24fb72d7a43fbf23463
Lacey Weaver <jlweave89@gmail.com>
2023-03-02 13:09:00 -0700

    Fix buggy flash method

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -1,12 +1,12 @@
 class UserRecipesController < ApplicationController
   def create
     Faraday.post("https://babe-whats-for-dinner-be.herokuapp.com/api/v1/user_recipes?uid=#{params[:uid]}&recipe_id=#{params[:recipe_id]}&recipe_name=#{params[:recipe_name]}")
-    if response.status != 201
+    if response.status == 201
+       @saved_recipes = UserRecipesFacade.get_recipes(current_user.uid)
+       redirect_to dashboard_path
+    else 
       flash[:errors] = "You already have this recipe saved"
       redirect_to discover_path(params[:recipe_id])
-    else
-      @saved_recipes = UserRecipesFacade.get_recipes(current_user.uid)
-      redirect_to dashboard_path
     end
   end
 
