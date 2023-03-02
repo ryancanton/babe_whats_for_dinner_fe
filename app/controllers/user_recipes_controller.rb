3f5ca38e86d56e1bf4fc35efe1103cab4bcfcbbe
Bryan Flanagan <bflanagan78@yahoo.com>
2023-03-02 13:51:33 -0700

    debugging: add response to dashboard

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -2,7 +2,7 @@ class UserRecipesController < ApplicationController
   def create
     Faraday.post("https://babe-whats-for-dinner-be.herokuapp.com/api/v1/user_recipes?uid=#{params[:uid]}&recipe_id=#{params[:recipe_id]}&recipe_name=#{params[:recipe_name]}")
 
-    if response.status == 422
+    if response.status == 422 || "422"
       flash[:errors] = "You already have this recipe saved"
       redirect_to discover_path(params[:recipe_id])
     else
