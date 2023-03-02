abc0174f2f169ffa4a501c568c4d9395d6728ca2
Bryan Flanagan <bflanagan78@yahoo.com>
2023-03-02 09:32:18 -0700

    refactor: Remove client_id from welcome index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @client_id = "f52797da1ca750c9f089"
-    # @client_secret = "226cd6ab93811f1ccabae4a1d9f9aa005140c1e1"
+    @client_id = ENV['client_id']
   end
 end
