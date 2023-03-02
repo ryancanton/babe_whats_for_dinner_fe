51a5a9c4734c83e631db68ded3e98205ca3e77e5
Bryan Flanagan <bflanagan78@yahoo.com>
2023-03-02 09:48:02 -0700

    refactor: update create method conditional...again...again

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -2,7 +2,7 @@ class UserRecipesController < ApplicationController
   def create
     Faraday.post("https://babe-whats-for-dinner-be.herokuapp.com/api/v1/user_recipes?uid=#{params[:uid]}&recipe_id=#{params[:recipe_id]}&recipe_name=#{params[:recipe_name]}")
     if response.status != 201
-      flash[:errors] = response.body[:errors]
+      flash[:errors] = "You already have this recipe saved"
       redirect_to discover_path(params[:recipe_id])
     else
       @saved_recipes = UserRecipesFacade.get_recipes(current_user.uid)
