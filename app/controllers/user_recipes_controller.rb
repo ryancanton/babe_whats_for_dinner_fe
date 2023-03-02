305d5fd4988699ca5eb2f8e22ae59b689fbfae91
Lacey Weaver <jlweave89@gmail.com>
2023-03-02 13:14:58 -0700

    Flash message status

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -1,7 +1,8 @@
 class UserRecipesController < ApplicationController
   def create
     Faraday.post("https://babe-whats-for-dinner-be.herokuapp.com/api/v1/user_recipes?uid=#{params[:uid]}&recipe_id=#{params[:recipe_id]}&recipe_name=#{params[:recipe_name]}")
-    if response.status == 201
+    require 'pry'; binding.pry
+    if response.status == 200
        @saved_recipes = UserRecipesFacade.get_recipes(current_user.uid)
        redirect_to dashboard_path
     else 
