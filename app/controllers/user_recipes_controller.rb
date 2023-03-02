9e8c834de204ba4652ba8e465a7a1da786e83755
Bryan Flanagan <bflanagan78@yahoo.com>
2023-03-02 09:38:33 -0700

    refactor: update create method to include redirect

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -3,6 +3,7 @@ class UserRecipesController < ApplicationController
     Faraday.post("https://babe-whats-for-dinner-be.herokuapp.com/api/v1/user_recipes?uid=#{params[:uid]}&recipe_id=#{params[:recipe_id]}&recipe_name=#{params[:recipe_name]}")
     if response.status == 422
       flash[:errors] = response.body
+      redirect_to discover_path(params[:recipe_id])
     else
       @saved_recipes = UserRecipesFacade.get_recipes(current_user.uid)
       redirect_to dashboard_path
