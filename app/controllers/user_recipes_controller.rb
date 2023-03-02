b2f0a21068eac554fe1b5d951fb960f35c13ef73
Lacey Weaver <jlweave89@gmail.com>
2023-03-02 13:22:13 -0700

    new flash

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -2,10 +2,11 @@ class UserRecipesController < ApplicationController
   def create
     Faraday.post("https://babe-whats-for-dinner-be.herokuapp.com/api/v1/user_recipes?uid=#{params[:uid]}&recipe_id=#{params[:recipe_id]}&recipe_name=#{params[:recipe_name]}")
     require 'pry'; binding.pry
-    if response.status != 200 || 201
+    if response.status != 201
       flash[:errors] = "You already have this recipe saved"
       redirect_to discover_path(params[:recipe_id])
     else
+      flash[:errors] = "You saved this recipe"
        @saved_recipes = UserRecipesFacade.get_recipes(current_user.uid)
        redirect_to dashboard_path
     end
