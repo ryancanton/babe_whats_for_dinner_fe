dc24bc99fb0852527bfb45aea46ed532dd4e9aa2
Bryan Flanagan <bflanagan78@yahoo.com>
2023-03-02 09:34:34 -0700

    refactor: update add recipes to not add duplicates

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -2,7 +2,7 @@ class UserRecipesController < ApplicationController
   def create
     Faraday.post("https://babe-whats-for-dinner-be.herokuapp.com/api/v1/user_recipes?uid=#{params[:uid]}&recipe_id=#{params[:recipe_id]}&recipe_name=#{params[:recipe_name]}")
     if response.status == 422
-      flash
+      flash[:errors] = response.body
     else
       @saved_recipes = UserRecipesFacade.get_recipes(current_user.uid)
       redirect_to dashboard_path
